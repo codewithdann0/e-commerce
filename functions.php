23a9b00a040635fbadd928e6dced4db243cb7c18
codewithdann0 <codewithdann028@gmail.com>
2024-06-11 05:23:05 -0700

    the home page add and the admin function is separted for for

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+// functions.php
+
+function isAdmin($user_id) {
+    // Check if the user ID is 21
+    return $user_id == 21;
+}
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Our E-Commerce Store</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<style>
+    .container{
+        background-image:url('/images/imge-1.jpg');
+        background-position: center;
+        background-size: cover;
+}
+     
+</style>
+<body>
+
+    <div class="container">
+        <h1>EthioBuy E-Commerce Store</h1>
+        <p>Discover the latest trends and best deals on our platform. Shop now for a wide range of products!</p>
+        <p>With fast shipping and secure payment options, shopping with us is convenient and hassle-free.</p>
+        <p>Join our community of satisfied customers today and experience the ultimate online shopping experience.</p>
+    </div>
+</body>
+</html>
+<?php require 'footer.php'; ?>
