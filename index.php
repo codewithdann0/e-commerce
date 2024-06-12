af0673c0e7273946f70380f92ab342a94de4a64e
codewithdann0 <codewithdann028@gmail.com>
2024-06-12 05:27:49 -0700

    the home page is made responsive

--- index.php
+++ index.php
@@ -7,20 +7,16 @@ require 'header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Our E-Commerce Store</title>
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/index.css?v=1.0.13">
 </head>
-<style>
-    .container{
-        background-image:url('/images/imge-1.jpg');
-        background-position: center;
-        background-size: cover;
-}
-     
-</style>
-<body>
 
+<body>
+   <h1>EthioBuy E-Commerce Store</h1>
+     <div class="box">
+    <img src="https://ewm.swiss/application/files/8216/1597/9679/E-commerce_web_design_EWM_SA_Digital_Agency_Geneva.jpg" alt="">
+     </div>
     <div class="container">
-        <h1>EthioBuy E-Commerce Store</h1>
+        <p><a href="product_list.php">EthioBuy E-Commerce Store</a></p>
         <p>Discover the latest trends and best deals on our platform. Shop now for a wide range of products!</p>
         <p>With fast shipping and secure payment options, shopping with us is convenient and hassle-free.</p>
         <p>Join our community of satisfied customers today and experience the ultimate online shopping experience.</p>
